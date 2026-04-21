a2da203ef98d174c452e7ccd0072a568bdfa7767
bdvaldez79 <bdvaldez@gmail.com>
2026-04-20 21:21:26 -0400

    Update plantFilter.ts

--- plantFilter.ts
+++ plantFilter.ts
@@ -1,1 +1,22 @@
+export const PLANT_KEYWORDS = [
+  "plant",
+  "plants",
+  "tree",
+  "trees",
+  "forest",
+  "botany",
+  "tropical",
+  "soil",
+  "carbon",
+  "biodiversity",
+  "ecosystem",
+  "climate",
+  "horticulture",
+  "orchid",
+  "agriculture",
+];
 
+export function isPlantArticle(text: string) {
+  const lower = text.toLowerCase();
+  return PLANT_KEYWORDS.some((word) => lower.includes(word));
+}
