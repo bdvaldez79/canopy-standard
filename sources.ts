f3154d134e66f08cd5ba4dcd723117f766f90500
bdvaldez79 <bdvaldez@gmail.com>
2026-04-20 21:20:15 -0400

    Update sources.ts

--- sources.ts
+++ sources.ts
@@ -1,1 +1,22 @@
-
+export const SOURCES = [
+  {
+    name: "BBC",
+    rss: "http://feeds.bbci.co.uk/news/science_and_environment/rss.xml",
+  },
+  {
+    name: "Nature",
+    rss: "https://www.nature.com/nature.rss",
+  },
+  {
+    name: "AP News",
+    rss: "https://apnews.com/hub/climate-and-environment/rss",
+  },
+  {
+    name: "Science News",
+    rss: "https://www.sciencenews.org/feed",
+  },
+  {
+    name: "National Geographic",
+    rss: "https://www.nationalgeographic.com/content/natgeo/en_us/index.rss",
+  },
+];
